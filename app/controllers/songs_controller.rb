65a1f7d871bff21efa58b564bc2deae56a4af884
tonitdiep <adiep123@gmail.com>
2020-11-16 22:43:41 -0500

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,5 +1,9 @@
-class SongsController < Sinatra::Base
+require 'rack-flash'
+require 'pry'
+class SongsController < ApplicationController
+    use Rack::Flash
     get '/songs' do 
+        # binding.pry
         @songs = Song.all
         erb :'/songs/index'
     end
--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,7 +1,14 @@
 class Artist < ActiveRecord::Base
-  has_many :song_genres
-  has_many :songs, through: :song_genres
-  def initialize(name:)
-    name = @name
+
+  has_many :songs
+  has_many :genres, through: :songs
+
+  def slug
+    self.name.downcase.gsub(" ", "-")
   end
+
+  def self.find_by_slug(slug)
+    Artist.all.find {|name| slug == name.slug}
+  end
+
 end
